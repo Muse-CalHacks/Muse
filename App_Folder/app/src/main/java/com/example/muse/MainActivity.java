84655d0c9bf48b46d2c03d40c6f6e92c912001d4
Krishaanth <krish100@my.yorku.ca>
2020-04-25 15:18:22 -0400

    Api response works
    Gives back a full json file

--- App_Folder/app/src/main/java/com/example/muse/MainActivity.java
+++ App_Folder/app/src/main/java/com/example/muse/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.muse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 import java.util.*;
 //import
 
@@ -14,17 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /*
-    OkHttpClient client = new OkHttpClient();
 
-    Request request = new Request.Builder()
-            .url("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem")
-            .get()
-            .addHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com")
-            .addHeader("x-rapidapi-key", "f501cb6e2fmsh6da6dc9b2afed6ep16156ejsn8a3f63043162")
-            .build();
+    public static String deezer() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem")
+                .get()
+                .addHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "f501cb6e2fmsh6da6dc9b2afed6ep16156ejsn8a3f63043162")
+                .build();
 
-    Response response = client.newCall(request).execute();
-    */
+        try(Response response = client.newCall(request).execute()){
+            return response.body().string();
+        } catch (IOException x){
+            return "No response";
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String ret = deezer();
+            System.out.println(ret);
+        } catch(IOException x){
+            System.out.println("Error no response (psvm)");
+        }
+    }
+
      
 }
